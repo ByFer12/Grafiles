c5e212630b03363234e54bbf9a8ba38588b4349b
Byron Torres <ajxupbyron@gmail.com>
2024-10-31 12:21:57 -0600

    grafiles terminado

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite el acceso desde fuera del contenedor
+    port: 5173, // Asegúrate de que este sea el puerto que usas
+  },
 })
